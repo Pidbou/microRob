d628aec480cef57103a05b502cd796eecb599ee9
Pidbou <99590834+Pidbou@users.noreply.github.com>
2022-03-07 19:40:05 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-function SetL293in (num: number, num2: number, num3: number, num4: number) {
+namespace microRob {
+export function SetL293in (num: number, num2: number, num3: number, num4: number) {
     pins.digitalWritePin(DigitalPin.P13, num)
     pins.digitalWritePin(DigitalPin.P14, num2)
     pins.digitalWritePin(DigitalPin.P15, num3)
     pins.digitalWritePin(DigitalPin.P16, num4)
 }
-function SetMotor2Speed (speed: number) {
+export function SetMotor2Speed (speed: number) {
     if (speed < 0) {
         pins.digitalWritePin(DigitalPin.P15, 1)
         pins.digitalWritePin(DigitalPin.P16, 0)
@@ -17,7 +18,7 @@ function SetMotor2Speed (speed: number) {
     }
     pins.analogWritePin(AnalogPin.P2, Math.round(Math.abs(speed) / 100 * 1023))
 }
-function SetMotor1Speed (speed: number) {
+export function SetMotor1Speed (speed: number) {
     if (speed < 0) {
         pins.digitalWritePin(DigitalPin.P13, 1)
         pins.digitalWritePin(DigitalPin.P14, 0)
@@ -29,4 +30,5 @@ function SetMotor1Speed (speed: number) {
         pins.digitalWritePin(DigitalPin.P14, 0)
     }
     pins.analogWritePin(AnalogPin.P1, Math.round(Math.abs(speed) / 100 * 1023))
+}
 }
